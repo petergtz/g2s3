023fd65991df9ff3af2aa744d59352b42d5ecfab
Peter Goetz <pego@amazon.com>
2022-10-20 23:01:26 +0200

    Fix binary name and pass folder and bucket via CLI args

--- src/bin/back-up-drive-folder.rs
+++ src/bin/back-up-drive-folder.rs
@@ -5,22 +5,27 @@ use error_chain::ChainedError;
 use google_drive::oauth2::read_authorized_user_secret;
 use log::{error, info};
 
-use google_photos_backup::{back_up, create_aus_from_env_vars, set_up_logging};
-use google_photos_backup::errors;
+use google_backup_to_s3::{back_up, create_aus_from_env_vars, set_up_logging};
+use google_backup_to_s3::errors;
 
 use errors::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     set_up_logging();
-
     info!("Starting");
+
+    let args: Vec<String> = std::env::args().collect();
+    assert_eq!(args.len(), 3);
+
+    let drive_folder_name = &args[1];
+    let bucket_name = &args[2];
 
     let authorized_user_secret = match create_aus_from_env_vars() {
         Ok(authorized_user_secret) => authorized_user_secret,
         Err(_) => read_authorized_user_secret("private/authorized_user_secret.json").await.unwrap()
     };
-    let result = back_up(authorized_user_secret).await;
+    let result = back_up(authorized_user_secret, drive_folder_name, bucket_name).await;
     if let Err(ref e) = result {
         error!("{}", e.display_chain());
         ::std::process::exit(1);
--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,6 @@ use google_drive::hyper::client::HttpConnector;
 use google_drive::hyper_rustls::HttpsConnector;
 use google_drive::oauth2;
 use google_drive::oauth2::authorized_user::AuthorizedUserSecret;
-use log::{error, info};
 use tokio::sync::mpsc;
 
 use errors::Result;
@@ -28,7 +27,7 @@ use errors::ResultExt;
 pub mod errors;
 
 
-pub async fn back_up(aus: AuthorizedUserSecret) -> Result<()> {
+pub async fn back_up(aus: AuthorizedUserSecret, drive_folder_name: &str, bucket_name: &str) -> Result<()> {
     let hub = Arc::new(create_drive_hub(aus).await);
     // let s3 = Arc::new(aws_sdk_s3::Client::new(
     //     &aws_config::from_env().region(RegionProviderChain::default_provider())
@@ -37,17 +36,21 @@ pub async fn back_up(aus: AuthorizedUserSecret) -> Result<()> {
         &aws_config::from_env().region("eu-west-1").load().await));
 
     let files = hub.files().list().
-        q("name = 'Takeout' and mimeType = 'application/vnd.google-apps.folder' and 'root' in \
-        parents").
-        param("fields", "files(id,name,parents,size)").doit().await.chain_err(|| "Could not find Takeout folder in drive.")?;
+        q(&*format!("name = '{drive_folder_name}' and \
+        mimeType = 'application/vnd.google-apps.folder' and \
+        'root' in parents")).
+        param("fields", "files(id,name,parents,size)").
+        doit().await.
+        chain_err(|| format!("Could not find {drive_folder_name} folder in drive."))?;
     assert_eq!(files.1.files.as_ref().unwrap().len(), 1);
-    let takeout_folder_id = files.1.files.as_ref().unwrap().first().as_ref().unwrap().id.as_ref().unwrap();
+    let folder_id = files.1.files.as_ref().unwrap().first().as_ref().unwrap().id.as_ref().unwrap();
 
     let mut page_token: Option<String> = None;
 
     let mut files = vec![];
     loop {
-        let file_list_response = list_files_in_takeout_folder(&hub, takeout_folder_id, &mut page_token).await.chain_err(|| "Could not list files in Takeout folder in drive.")?.1;
+        let file_list_response = list_files_in_folder(&hub, folder_id, &mut page_token).await.
+            chain_err(|| format!("Could not list files in {drive_folder_name} folder in drive."))?.1;
         for file in file_list_response.files.as_ref().unwrap() {
             files.push(file.clone());
         }
@@ -63,7 +66,7 @@ pub async fn back_up(aus: AuthorizedUserSecret) -> Result<()> {
             let (hub, s3) = (hub.clone(), s3.clone());
             let tx = tx.clone();
             async move {
-                let result = copy_file(&*hub, &*s3, file.clone()).await;
+                let result = copy_file(&*hub, &*s3, file.clone(), bucket_name).await;
                 tx.send(result).unwrap();
             }
         },
@@ -75,7 +78,7 @@ pub async fn back_up(aus: AuthorizedUserSecret) -> Result<()> {
         match r {
             Ok(_) => {}
             Err(e) => {
-                error!("Error during copy: {}", e);
+                log::error!("Error during copy: {}", e);
                 result = Err(e);
             }
         }
@@ -83,12 +86,12 @@ pub async fn back_up(aus: AuthorizedUserSecret) -> Result<()> {
     result
 }
 
-async fn list_files_in_takeout_folder(hub: &DriveHub<HttpsConnector<HttpConnector>>,
-                                      takeout_folder_id: &String,
-                                      page_token: &mut Option<String>)
-                                      -> google_drive3::Result<(Response<Body>, FileList)> {
+async fn list_files_in_folder(hub: &DriveHub<HttpsConnector<HttpConnector>>,
+                              folder_id: &String,
+                              page_token: &mut Option<String>)
+                              -> google_drive3::Result<(Response<Body>, FileList)> {
     let mut list_query = hub.files().list().
-        q(format!("'{}' in parents", takeout_folder_id).as_str()).
+        q(format!("'{}' in parents", folder_id).as_str()).
         param("fields", "nextPageToken,files(id,name,parents,md5Checksum,size)");
     if page_token.is_some() {
         list_query = list_query.page_token(page_token.as_ref().unwrap().as_str());
@@ -122,23 +125,26 @@ pub fn create_aus_from_env_vars() -> Result<AuthorizedUserSecret> {
     })
 }
 
-async fn copy_file(hub: &DriveHub<HttpsConnector<HttpConnector>>, s3: &Client, file: google_drive3::api::File) -> Result<()> {
+async fn copy_file(hub: &DriveHub<HttpsConnector<HttpConnector>>,
+                   s3: &Client,
+                   file: google_drive3::api::File,
+                   bucket_name: &str) -> Result<()> {
     let filename = file.name.as_ref().unwrap();
-    info!("Copying file {}", filename);
+    log::info!("Copying file {}", filename);
     let start_time = Instant::now();
 
     let file_content = hub.files()
         .get(file.id.as_ref().unwrap()).param("alt", "media")
         .doit()
-        .await.chain_err(|| format!("Could not download file contents from drive for {}.", filename))?.0;
+        .await.chain_err(|| format!("Could not download file contents from drive for {filename}."))?.0;
     assert!(file_content.status().is_success());
-    let _s3_response = s3.put_object().bucket("pego-google-photos-backup")
+    let _s3_response = s3.put_object().bucket(bucket_name)
         .key(filename)
         .content_md5(base64::encode(hex::decode(file.md5_checksum.as_ref().unwrap()).unwrap()))
         .body(ByteStream::new(SdkBody::from(file_content.into_body())))
         .storage_class(StorageClass::DeepArchive)
-        .send().await.chain_err(|| format!("Could not upload file contents for {}", filename))?;
-    info!("Throughput for file {} of size {} B: {} B/s",
+        .send().await.chain_err(|| format!("Could not upload file contents for {filename}"))?;
+    log::info!("Throughput for file {} of size {} B: {} B/s",
                 filename,
                 file.size.as_ref().unwrap(),
                 file.size.as_ref().unwrap().parse::<f64>().unwrap()/start_time.elapsed().as_secs() as f64);
